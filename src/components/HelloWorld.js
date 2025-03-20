c0de29de3da523187085069a81d425ba5a3e2d64
Ajit Kamble <ajitkamble@outlook.in>
2025-03-20 20:34:32 +0000

    HelloWorld Component added

---
+++ src/components/HelloWorld.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const HelloWorld = () => {
+    return (
+        <div>
+            <h4>Assignment 1</h4>
+            <h1>Hello World!</h1>
+            <h4>----------------------------------------</h4>
+        </div>
+    );
+};
+
+export default HelloWorld;
